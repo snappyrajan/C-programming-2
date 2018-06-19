ab48e916171905e79f88399f158c2b0c315db9ff
balasachin <38652822+balasachin@users.noreply.github.com>
2018-06-19 10:40:26 +0000

    Update evenodd.c

--- GUVI/evenodd.c
+++ GUVI/evenodd.c
@@ -3,18 +3,12 @@ int main()
 {
   long int n;
   scanf("%ld",&n);
-  if(n>0 && n<=100000)
-  {
-    if(n%2==0)
-    {
-      printf("even");
-    }
-    else
-    {
-      printf("odd");
-    }
-  else
+  if(n%2==0)
    {
-      printf("invalid");
-    }
+     printf("even");
+   }
+   else
+   {
+     printf("odd");
+   }
 }
