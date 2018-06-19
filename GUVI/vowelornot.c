245e64ba01ce53bc6ab02598860c447a33af4f38
balasachin <38652822+balasachin@users.noreply.github.com>
2018-06-19 10:59:00 +0000

    Update vowelornot.c

--- GUVI/vowelornot.c
+++ GUVI/vowelornot.c
@@ -4,7 +4,11 @@ int main()
 char a;
 scanf("%c",&a);
 if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
-   {printf("vowel");}
+   {
+    printf("vowel");
+   }
 else
-printf("consonant");
+   {
+      printf("consonant");
+   }
 }
